c8e6b3eb378d9d66e3f0ee4733f4d9a4109f7461
TijolinhoisReal <152935076+TijolinhoisReal@users.noreply.github.com>
2024-04-30 18:45:49 -0300

    Add files via upload

---
+++ rickmorty/src/views/AboutView.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        ALUNO
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+      <div class="accordion-body">
+        <p><strong>ALUNO: </strong> Juan Alfredo Perozo Romeiro</p>
+        <p><strong>RA: </strong> 1963281</p>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+        CURSO
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+      <div class="accordion-body">
+        <p><strong>CURSO: </strong> Analise e Desenvolvimento de Sistemas</p>
+        <p><strong>TERMO: </strong> 3º Termo (2024)</p>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+        UNIVERSIDADE
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+      <div class="accordion-body">
+        <p><strong>UNIVERSIDADE: </strong> Universidade De Marilia (UNIMAR)</p>
+        <p><strong>CONTATO: </strong> falecom@unimar.br</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
